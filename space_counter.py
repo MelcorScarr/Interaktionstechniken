d97b72a6ecbcd9cbca5e784d8bf1de58fca48036
Christoph Härtl <christoph.haertl@stud.uni-regensburg.de>
2018-05-02 14:35:47 +0200

    added noise functionality

--- space_counter.py
+++ space_counter.py
@@ -3,24 +3,23 @@
 
 
 import sys
-import time
 import csv
 import random
 from PyQt5 import QtGui, QtWidgets, QtCore, QtMultimedia
 
 
 class ExperimentRecorder(QtWidgets.QWidget):
-    def __init__(self, participant, experiments, delay):
+    def __init__(self, participant, experiments, delay, app):
         super().__init__()
+        self.app = app
         self.participant = participant
         self.experiments = experiments
         self.delay = int(delay)
         self.counter = -1
+        self.cleanwindow = False
         self.timer = QtCore.QElapsedTimer()
         self.timer.start()
-        # See https://stackoverflow.com/questions/42845981/trying-adding-a-sound-event-using-qmediaplayer
         self.player = QtMultimedia.QSound("whitenoise.wav")
-
         self.initUI()
 
         self.colours = ["Red", "Green"]
@@ -56,13 +55,14 @@ class ExperimentRecorder(QtWidgets.QWidget):
             else:
                 correct = False
         self.log.append(
-            [self.participant, self.stimulus, self.attentive, self.distraction, buttonPressed, correct, timestamp,self.timer.elapsed()])
-        print(self.log)
+            [self.participant, self.stimulus, self.attentive, self.distraction, buttonPressed, correct, timestamp,
+             self.timer.elapsed()])
 
     def output_log(self):
         with open("output.csv", "w", newline='') as f:
             writer = csv.writer(f)
             writer.writerows(self.log)
+        sys.exit(self.app.exec_())
 
     def initUI(self):
         # set the text property of the widget we are inheriting
@@ -75,67 +75,83 @@ class ExperimentRecorder(QtWidgets.QWidget):
     def keyPressEvent(self, ev):
         if ev.key() == QtCore.Qt.Key_Q:
             self.log_time('Q')
-            self.counter += 1
-            print(self.counter)
-            print(len(self.experiments))
         elif ev.key() == QtCore.Qt.Key_P:
             self.log_time('P')
-            self.counter += 1
-        time.sleep(self.delay / 1000)
+        self.cleanwindow = True
+        self.update()
+
+        QtCore.QTimer.singleShot(self.delay, self.nextExperiment)
+
+    def nextExperiment(self):
+        print("Hi?")
+        self.counter += 1
+        self.cleanwindow = False
         self.timer.start()
         if self.counter == len(self.experiments):
             self.output_log()
         else:
             if self.experiments[self.counter][1] == 'D':
+                self.distraction = True
                 self.player.play()
             else:
+                self.distraction = False
                 self.player.stop()
             self.update()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
-        self.drawText(event, qp)
         self.drawRect(event, qp)
+        self.drawText(event, qp)
         qp.end()
 
     def drawText(self, event, qp):
-        if self.counter == -1:
+        if not self.cleanwindow:
+            if self.counter == -1:
+                qp.setPen(QtGui.QColor("Black"))
+                qp.setFont(QtGui.QFont('Decorative', 24))
+                qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q or P to start!")
+            elif self.experiments[self.counter][0] == 'A':
+                self.attentive = True
+                qp.setPen(QtGui.QColor("Black"))
+                qp.setFont(QtGui.QFont('Decorative', 24))
+                qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q for english, or P for german.")
+                qp.setPen(QtGui.QColor("Black"))
+                qp.setFont(QtGui.QFont('Decorative', 32))
+                if random.randint(0, 1):
+                    random_word = random.choice(self.englishWords)
+                    self.stimulus = random_word
+                    qp.drawText(event.rect(), QtCore.Qt.AlignCenter, random_word)
+                else:
+                    random_word = random.choice(self.germanWords)
+                    self.stimulus = random_word
+                    qp.drawText(event.rect(), QtCore.Qt.AlignCenter, random_word)
+            elif self.experiments[self.counter][0] == 'P':
+                self.attentive = True
+                qp.setPen(QtGui.QColor("Black"))
+                qp.setFont(QtGui.QFont('Decorative', 24))
+                qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q for Red, or P for Green.")
+        else:
             qp.setPen(QtGui.QColor("Black"))
             qp.setFont(QtGui.QFont('Decorative', 24))
-            qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q or P to start!")
-        elif self.experiments[self.counter][0] == 'A':
-            self.attentive = True
-            qp.setPen(QtGui.QColor("Black"))
-            qp.setFont(QtGui.QFont('Decorative', 24))
-            qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q for english, or P for german.")
-            qp.setPen(QtGui.QColor("Black"))
-            qp.setFont(QtGui.QFont('Decorative', 32))
-            if random.randint(0, 1):
-                random_word = random.choice(self.englishWords)
-                self.stimulus = random_word
-                qp.drawText(event.rect(), QtCore.Qt.AlignCenter, random_word)
-            else:
-                random_word = random.choice(self.germanWords)
-                self.stimulus = random_word
-                qp.drawText(event.rect(), QtCore.Qt.AlignCenter, random_word)
-        elif self.experiments[self.counter][0] == 'P':
-            self.attentive = True
-            qp.setPen(QtGui.QColor("Black"))
-            qp.setFont(QtGui.QFont('Decorative', 24))
-            qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Press Q for Red, or P for Green.")
+            qp.drawText(event.rect(), QtCore.Qt.AlignHCenter, "Get ready for the next one...")
 
     def drawRect(self, event, qp):
-        if self.counter > -1 and self.experiments[self.counter][0] == 'P':
-            self.attentive = False
-            rect = QtCore.QRect(250, 250, 100, 100)
-            if random.randint(0, 1):
-                self.stimulus = "Green"
-                qp.setBrush(QtGui.QColor("Green"))
-            else:
-                self.stimulus = "Red"
-                qp.setBrush(QtGui.QColor("Red"))
-            qp.drawRoundedRect(rect, 10.0, 10.0)
+        if not self.cleanwindow:
+            if self.counter > -1 and self.experiments[self.counter][0] == 'P':
+                self.attentive = False
+                rect = QtCore.QRect(250, 250, 100, 100)
+                if random.randint(0, 1):
+                    self.stimulus = "Green"
+                    qp.setBrush(QtGui.QColor("Green"))
+                else:
+                    self.stimulus = "Red"
+                    qp.setBrush(QtGui.QColor("Red"))
+                qp.drawRoundedRect(rect, 10.0, 10.0)
+        else:
+            rect = QtCore.QRect(0, 0, 600, 600)
+            qp.setBrush(QtGui.QColor("White"))
+            qp.drawRect(rect)
 
 
 def main():
@@ -145,7 +161,7 @@ def main():
     for i in input.readlines():
         values.append(i.split(":", 1)[1].replace('\n', '').replace(' ', ''))
     # variable is never used, class automatically registers itself for Qt main loop:
-    space = ExperimentRecorder(values[0], values[1].split(','), values[2])
+    space = ExperimentRecorder(values[0], values[1].split(','), values[2], app)
     sys.exit(app.exec_())
 
 
